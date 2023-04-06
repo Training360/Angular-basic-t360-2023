354bf173831a5d2ba9ee957c4c623158ab45df13
Evelin <ladyblue@freemail.hu>
2023-04-06 18:23:20 +0200

    Components w/ Bootstrap

---
+++ projects/video-player/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { SearchBarComponent } from './components/search-bar/search-bar.component';
+import { VideoDetailsComponent } from './components/video-details/video-details.component';
+import { VideoItemComponent } from './components/video-item/video-item.component';
+import { VideoListComponent } from './components/video-list/video-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SearchBarComponent,
+    VideoDetailsComponent,
+    VideoItemComponent,
+    VideoListComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
