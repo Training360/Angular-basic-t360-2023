ee8a508c0e39a4dad9a6673c97ecbb90d2b1a199
Evelin <ladyblue@freemail.hu>
2023-04-14 18:04:51 +0200

    Guitar model

---
+++ projects/webshop/src/app/model/guitar.ts
@@ -0,0 +1,9 @@
+export class Guitar {
+  [key: string]: any;
+  id: number = 0;
+  title: string = '';
+  description: string = '';
+  price: number = 0;
+  rating: number = 0;
+  stock: number = 0;
+}
