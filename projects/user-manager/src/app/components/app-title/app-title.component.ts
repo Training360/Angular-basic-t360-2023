cef0b368003ce1fc4124c6e30b32bf8d7e44e0ee
Evelin <ladyblue@freemail.hu>
2023-04-26 13:00:10 +0200

    @Input()

--- projects/user-manager/src/app/components/app-title/app-title.component.ts
+++ projects/user-manager/src/app/components/app-title/app-title.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-app-title',
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app-title.component.scss']
 })
 export class AppTitleComponent {
-
+  @Input() title: string = '';
 }
