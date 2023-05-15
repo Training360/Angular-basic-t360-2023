efc68478aee9d2d69f6ea488a8f286bddc839db9
Evelin <ladyblue@freemail.hu>
2023-05-15 17:42:28 +0200

    Price

--- projects/webshop/src/app/components/guitar-price/guitar-price.component.ts
+++ projects/webshop/src/app/components/guitar-price/guitar-price.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-guitar-price',
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./guitar-price.component.scss']
 })
 export class GuitarPriceComponent {
+  @Input() price: number = 0;
 
+  formatPrice(price: number): string {
+    return price.toLocaleString('en-US', { style: 'currency', currency: 'EUR' })
+  }
 }
+
