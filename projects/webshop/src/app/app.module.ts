c30ff50851e0b5f6aec12ee7d43acdb20814fdfe
Evelin <ladyblue@freemail.hu>
2023-05-15 15:30:44 +0200

    AppModule with components

--- projects/webshop/src/app/app.module.ts
+++ projects/webshop/src/app/app.module.ts
@@ -10,13 +10,28 @@ import { AppComponent } from './app.component';
 import { MainNavigationComponent } from './components/main-navigation/main-navigation.component';
 import { GuitarsListComponent } from './components/guitars-list/guitars-list.component';
 import { CartComponent } from './components/cart/cart.component';
-
+import { GuitarItemComponent } from './components/guitar-item/guitar-item.component';
+import { GuitarImageComponent } from './components/guitar-image/guitar-image.component';
+import { GuitarTitleComponent } from './components/guitar-title/guitar-title.component';
+import { GuitarPriceComponent } from './components/guitar-price/guitar-price.component';
+import { GuitarRatingComponent } from './components/guitar-rating/guitar-rating.component';
+import { GuitarStockComponent } from './components/guitar-stock/guitar-stock.component';
+import { GuitarDescriptionComponent } from './components/guitar-description/guitar-description.component';
+import { CartButtonComponent } from './components/cart-button/cart-button.component';
 @NgModule({
   declarations: [
     AppComponent,
     MainNavigationComponent,
     GuitarsListComponent,
-    CartComponent
+    CartComponent,
+    GuitarItemComponent,
+    GuitarImageComponent,
+    GuitarTitleComponent,
+    GuitarPriceComponent,
+    GuitarRatingComponent,
+    GuitarStockComponent,
+    CartButtonComponent,
+    GuitarDescriptionComponent
   ],
   imports: [
     BrowserModule,
