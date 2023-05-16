3f750d011c905c22a1a204e77443026c68956ad1
Evelin <ladyblue@freemail.hu>
2023-05-16 15:31:52 +0200

    Add guitars to cart w/ service

--- projects/webshop/src/app/components/cart-button/cart-button.component.ts
+++ projects/webshop/src/app/components/cart-button/cart-button.component.ts
@@ -1,5 +1,6 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, inject } from '@angular/core';
 import { Guitar } from 'src/app/model/guitar';
+import { CartService } from 'src/app/service/cart.service';
 
 @Component({
   selector: 'app-cart-button',
@@ -8,17 +9,23 @@ import { Guitar } from 'src/app/model/guitar';
 })
 export class CartButtonComponent {
 
+  @Input() guitar: Guitar = new Guitar();
+  cartService: CartService = inject(CartService);
+
   count: number = 0;
+  buttonText: string = 'Add to cart';
 
-  handleDescreaseCountClick(): number {
-    return this.count -= 1;
+  handleDescreaseCountClick() {
+    this.count -= 1;
   }
 
-  handleIncreaseCountClick(): number {
-    return this.count += 1;
+  handleIncreaseCountClick() {
+    this.count += 1;
   }
 
-  handleCartButtonClick() {
-    // Add cart button logic here
+  handleCartButtonClick(guitar: Guitar) {
+    this.cartService.addItemToCart(guitar, this.count);
+    this.buttonText = 'Update cart';
+    console.log(this.cartService.cart);
   }
 }
