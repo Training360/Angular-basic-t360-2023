bf812a5c7a8a6e12af11b467ccea804adc1ce292
Evelin <ladyblue@freemail.hu>
2023-05-17 11:21:28 +0200

    Cart service CRUD

--- projects/webshop/src/app/service/cart.service.ts
+++ projects/webshop/src/app/service/cart.service.ts
@@ -1,12 +1,40 @@
 import { Injectable } from '@angular/core';
 
+interface CartItem {
+  id: number;
+  count: number;
+}
+
 @Injectable({
   providedIn: 'root'
 })
+
 export class CartService {
-  cart: any[] = [];
+  cart: CartItem[] = [];
+
+  private getItemIndexById(id: number): number {
+    return this.cart.findIndex(item => item.id === id);
+  }
+
+  getItemById(id: number): CartItem | undefined {
+    return this.cart.find(item => item.id === id);
+  }
 
   addItemToCart(item: any, count: number) {
     this.cart.push({ ...item, count });
+  }
+
+  changeItemCount(id: number, count: number): void {
+    const index = this.getItemIndexById(id);
+    if (index !== -1) {
+      this.cart[index].count = count;
+    }
+  }
+
+  removeFromCart(id: number): void {
+    const index = this.getItemIndexById(id);
+    if (index !== -1) {
+      this.cart.splice(index, 1);
+    }
   }
 }
