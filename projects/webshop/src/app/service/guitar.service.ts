9588cbce9c93db624d12a1a37e15fc2f373eb8d6
Evelin <ladyblue@freemail.hu>
2023-05-17 19:27:48 +0200

    inject()

--- projects/webshop/src/app/service/guitar.service.ts
+++ projects/webshop/src/app/service/guitar.service.ts
@@ -1,5 +1,5 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Guitar } from '../model/guitar';
 
@@ -9,8 +9,7 @@ import { Guitar } from '../model/guitar';
 export class GuitarService {
   usersURL: string = 'http://localhost:3002/guitars';
 
-  constructor(
-    private http: HttpClient) { }
+  http: HttpClient = inject(HttpClient);
 
   getAll(): Observable<Guitar[]> {
     return this.http.get<Guitar[]>(this.usersURL);
