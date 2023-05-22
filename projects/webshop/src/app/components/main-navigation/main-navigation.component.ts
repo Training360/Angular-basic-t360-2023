6046f1ef646b24464c5e722abea3f24411435695
Evelin <ladyblue@freemail.hu>
2023-05-22 16:39:21 +0200

    Lifecycle hook demo

--- projects/webshop/src/app/components/main-navigation/main-navigation.component.ts
+++ projects/webshop/src/app/components/main-navigation/main-navigation.component.ts
@@ -4,21 +4,24 @@ import { CartService } from 'src/app/service/cart.service';
 @Component({
   selector: 'app-main-navigation',
   templateUrl: './main-navigation.component.html',
-  styleUrls: ['./main-navigation.component.scss']
+  styleUrls: ['./main-navigation.component.scss'],
 })
 export class MainNavigationComponent {
-   @Input() message: string = '';
-   @Output() buttonWasClicked = new EventEmitter<void>();
-
-   onButtonClick() {
-     this.buttonWasClicked.emit();
-   }
+  @Input() message: string = '';
+  @Output() buttonWasClicked = new EventEmitter<void>();
 
   cartService: CartService = inject(CartService);
-
   cartContent = this.cartService.cart;
+
+  onButtonClick() {
+    this.buttonWasClicked.emit();
+  }
 
   totalCount(): number {
     return this.cartContent.reduce((prev, curr) => prev + curr.count, 0);
+  }
+
+  ngOnInit() {
+    console.log('I am invoked only once.');
   }
 }
