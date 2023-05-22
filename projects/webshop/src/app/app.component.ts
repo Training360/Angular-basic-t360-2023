154599932d8296732e44529305b2a04625ce421f
Evelin <ladyblue@freemail.hu>
2023-05-22 17:07:31 +0200

    Clean code

--- projects/webshop/src/app/app.component.ts
+++ projects/webshop/src/app/app.component.ts
@@ -7,10 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'webshop';
-
   currentMessage: string = '';
 
-  // Output
   buttonClickedMessage() {
     if (this.currentMessage === 'Hey, you have just clicked the button')
       this.currentMessage = '';
