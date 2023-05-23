f68896e42d2cd11f007092736a6e59689c1a36a0
Evelin <ladyblue@freemail.hu>
2023-05-23 15:30:52 +0200

    Cart service updated

--- projects/webshop/src/app/service/cart.service.ts
+++ projects/webshop/src/app/service/cart.service.ts
@@ -16,22 +16,18 @@ export class CartService {
     return this.cart.find(item => item.id === id);
   }
 
-  addItemToCart(item: any, count: number) {
+  addItemToCart(item: any, count: number): void {
     this.cart.push({ ...item, count });
   }
 
   changeItemCount(id: number, count: number): void {
     const index = this.getItemIndexById(id);
-    if (index !== -1) {
-      this.cart[index].count = count;
-    }
+    this.cart[index].count = count;
   }
 
   removeFromCart(id: number): void {
     const index = this.getItemIndexById(id);
-    if (index !== -1) {
-      this.cart.splice(index, 1);
-    }
+    this.cart.splice(index, 1);
   }
 
   get totalPrice(): number {
